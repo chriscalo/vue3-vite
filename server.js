6c70dc45bc0466126662ab6150bad21afe0f511a
Chris Calo <ccalo@google.com>
2020-05-25 11:37:48 -0400

    add production server (but still not fully correct)

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 import { createServer } from "vite";
 import Koa from "koa";
-import mount from "koa-mount";
+import koaMount from "koa-mount";
+import koaStatic from "koa-static";
 import api from "./api.js";
+
+const PORT = process.env.PORT || 8080;
+
+switch (process.env.NODE_ENV) {
+  case "development": {
+    createServer({
+      configureServer: [apiPlugin],
+    }).listen(PORT, () => {
+      console.log();
+      console.log(`Dev server running at:`);
+      console.log(`http://localhost:${PORT}/`);
+      console.log();
+    });
+    break;
+  }
+  
+  case "production": {
+    const app = new Koa();
+    app.use(koaStatic("dist"));
+    app.use(koaMount("/api", api));
+    app.listen(PORT, () => {
+      console.log();
+      console.log(`Prod server running at:`);
+      console.log(`http://localhost:${PORT}/`);
+      console.log();
+    });
+    break;
+  }
+  
+  default: {
+    throw `Unknown NODE_ENV value: ${process.env.NODE_ENV}`;
+    break;
+  }
+}
 
 function apiPlugin({
   root, // project root directory, absolute path
@@ -9,16 +44,5 @@ function apiPlugin({
   server, // raw http server instance
   watcher, // chokidar file watcher instance
 }) {
-  app.use(mount("/api", api));
+  app.use(koaMount("/api", api));
 }
-
-const PORT = process.env.PORT || 8080;
-
-createServer({
-  configureServer: [apiPlugin],
-}).listen(PORT, () => {
-  console.log();
-  console.log(`Dev server running at:`);
-  console.log(`http://localhost:${PORT}/`);
-  console.log();
-});
