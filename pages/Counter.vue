a6626b47f7ae5b33c935c2dcb3daa7c17859d5cc
Chris Calo <ccalo@google.com>
2020-05-25 01:55:19 -0400

    add more to counter

--- pages/Counter.vue
+++ pages/Counter.vue
@@ -1,14 +1,28 @@
 <template>
-  <button @click="count++">Clicks: {{ count }}</button>
+  <button @click="count--">−</button>
+  <span style="margin: 0.5rem;">{{ count }}</span>
+  <button @click="count++">+</button>
+  <button @click="reset">Reset</button>
 </template>
 
 <script>
+  import { ref } from "vue";
+  
+  function setup() {
+    const count = ref(0);
+    
+    function reset() {
+      count.value = 0;
+    }
+    
+    return {
+      count,
+      reset,
+    };
+  }
+  
   export default {
-    setup() {
-      return {
-        count: 0,
-      };
-    },
+    setup,
   };
 </script>
 
