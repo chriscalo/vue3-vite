a35b180624d6d510a04ae7b620e80c71cbfca2ef
Chris Calo <ccalo@google.com>
2020-06-27 01:53:12 -0400

    remove unused stuff

--- server/index.js
+++ server/index.js
@@ -1,20 +1,13 @@
-import { createServer } from "vite";
 import express from "express";
-import fallback from "express-history-api-fallback";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 import api from "./api.js";
 import ui from "./ui.js";
-
-const PORT = process.env.PORT || 8080;
-const VITE_DEV_SERVER_PORT = PORT + 1;
-const VITE_DEV_SERVER_ADDR = `http://localhost:${VITE_DEV_SERVER_PORT}`;
 
 const server = express();
 server.use("/api", api);
 server.use(ui);
 
-// the general server
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log();
   console.log(`Server running at:`);
